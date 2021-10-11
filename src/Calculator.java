8e8c6b654140fd596b58f78d6181508acd609cc9
vidya <vidya.kethuneni@gmail.com>
2021-10-11 14:56:34 +0530

    First Commit on calc

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Calculator {
+   public static void main(String[] ar) {
+	   int a = 10;
+	   int b = 20;
+	   System.out.println("Multiplication of 10 and 20 is: "+(a*b));
+	   System.out.println("Division of 20 by 10 is: "+(b/a));
+   }
+}
